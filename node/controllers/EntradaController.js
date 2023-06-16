86e8c0b1ae250f3834d337941265090bfb7bf051
Alan <alanalexxis3@gmail.com>
2023-06-16 12:39:31 -0500

    corregido nomProveedor

--- node/controllers/EntradaController.js
+++ node/controllers/EntradaController.js
@@ -19,7 +19,8 @@ export const getAllEntradas = async (req, res) => {
         costoTotal: entrada.costoTotal,
         fechaEntrada: entrada.fechaEntrada,
         numFactura: entrada.numFactura,
-        nomProductos: entrada.producto.descripcion,
+        nomProducto: entrada.producto.descripcion,
+        nomProveedor: entrada.proveedor.nomProveedor,
       }))
     );
   } catch (error) {
